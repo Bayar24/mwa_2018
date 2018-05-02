b157a4ff7fd171608ea27db002aad80e9fe0ebbd
bayar <bbayar@bitbucket.org>
2018-05-02 16:23:56 -0500

    Day2

--- Week1_Day2/exercise3.js
+++ Week1_Day2/exercise3.js
@@ -1,11 +1,43 @@
 ﻿function slow(callback){
-	for(let i=0;i<5e8;i++){}
+	setTimeout(()=>{
+        for(let i=0; i<= 5e8; i++){}
+    }, 0);
 	if(Math.random()>0.5){
 		return callback("Error", null);
 	}
-	callback(null, {id:12345});
+	return callback(null, {id:12345});
 }
 function exec(fn){
-	
+	const callback = function(err, data){
+		return err === null ? data : err;
+	}
+	const res = fn(callback);
+	return {
+        done: function(cb){
+            if(res !== "Error"){
+                cb(res);
+            }
+            return this;
+        },
+        fail: function(cb){
+            if(res === "Error"){
+                cb(res);
+            }
+            return this;
+        }
+    }
+	let obj = {};
+	fn(function(err, data){
+		obj.done = function(cb){
+			if(err ===null)
+				cb(data);
+			return this;
+		}
+		obj.fail = function(cb){
+			if(err !==null)
+				cb(err);
+			return this;
+		}
+	});
 }
 exec.slow().done(function(data){console.log(data);}).fail(function(err){console.log("Error: "+err);});
